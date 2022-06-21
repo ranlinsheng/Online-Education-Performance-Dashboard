0b8ece10fa3d7cc7c633e9ac82615423028bd94c
RAN LINSHENG <ranlinshenging@gmail.com>
2022-06-22 02:57:46 +0800

    Add files via upload

--- Education Shiny/server.R
+++ Education Shiny/server.R
@@ -1,10 +1,14 @@
 server <- function(input,output){
   df=read.csv("Data/ONLINE EDUCATION SYSTEM REVIEW.csv",stringsAsFactors = TRUE)
+  df1=read.csv("Data/before vs after.csv",stringsAsFactors = TRUE)
+  df2=read.csv("Data/SocialMedia vs Performance.csv",stringsAsFactors = TRUE)
+  
   Gender<- list("Male","Female")
   Homelocation<- list("Urban","Rural")
   Education<-list("School","Under Graduate", "Post Graduate")
   library(scales) 
   library(tigerstats)
+  library(plotly)
   
   Economicstatus<- list("Poor","Middle Class", "Rich")
   FamilySize<-list("1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11+")
@@ -12,14 +16,12 @@ server <- function(input,output){
   
   barplot_action<-eventReactive(input$plot,{filter(df,(Do.elderly.people.monitor.you.==input$mentor & Gender==input$gender))})
 
-  #########overview page  #####
-  #Scatterplot
-  output$plot1 <- renderPlot({
-    
-    After <- df$Performance.in.online
-    Before <- strtoi(substr(df$Average.marks.scored.before.pandemic.in.traditional.classroom,1,1))
-    plot(x=Before, y=After, xlab = "Performance Before Pandemic", ylab = "Performance During Pandemic", xlim = c(1,10), ylim = c(1,10), main = "Comparison of Education Performance Before and During Pandemic", pch=19)
-    lines(x=Before, y=Before, col="yellow", lwd=2.0)
+  #overview page
+  #Before vs After "Comparison of Education Performance Before and During Pandemic"
+  output$plot1 <- renderPlotly({
+    plot_ly(data = df1,x=~Performance.Before.Pandemic,y=~Performance.During.Pandemic
+            ,type = "scatter",mode = "markers",colors="blue",marker=list(size=df1$Count,opacity=0.5))
+
   })
   
   
@@ -33,7 +35,7 @@ server <- function(input,output){
     tb1 <- cbind(Education_Level,Difference)
     tb1 <- colPerc(xtabs(~ Difference+Education_Level,data=tb1))
     tb1 <- tb1[1:3,1:3]
-    barplot(tb1, col=colors()[c(23,2,12)],main = "Performance by Education Level",border="white", 
+    barplot(tb1, col=colors()[c(23,89,12)],main = "",border="white", 
             space=0.08, font.axis=2,xlab = "Education Level", ylab = "Percentage",
             legend.text = TRUE,args.legend = list(x="topright", bty = "n", inset=c(0, 1)))
   })
@@ -45,13 +47,9 @@ server <- function(input,output){
     Difference <- ifelse(d==0,"Maintain", ifelse(d>0,"Improved","Under"))
     tb2 <- cbind(Gender,Difference)
     tb2 <- xtabs(~ Difference+Gender,data=tb2)
-    tb2
     lab <- rownames(tb2)
-    lab
     tb2f <- tb2[1:3,1]
-    tb2f
     tb2m <- tb2[1:3,2]
-    tb2m
     pie(tb2f, labels=lab, main = "Female Education Performance", col = c("green","grey", "pink"),clockwise = T)
     legend("topright", c("Improved","Maintain","Under"), cex = 0.8, fill = c("green","grey", "pink"))
   })
@@ -64,33 +62,13 @@ server <- function(input,output){
     Difference <- ifelse(d==0,"Maintain", ifelse(d>0,"Improved","Under"))
     tb2 <- cbind(Gender,Difference)
     tb2 <- xtabs(~ Difference+Gender,data=tb2)
-    tb2
     lab <- rownames(tb2)
-    lab
     tb2f <- tb2[1:3,1]
-    tb2f
     tb2m <- tb2[1:3,2]
-    tb2m
     pie(tb2m, labels=lab, main = "Male Education Performance", col = c("green","grey", "pink"),clockwise = T)
     legend("topright", c("Improved","Maintain","Under"), cex = 0.8, fill = c("green","grey", "pink"))
-  })##############
-  
-  
-  #first performance tab  
-  output$violin<-renderPlot({
-    # df %>% filter(Do.elderly.people.monitor.you.==input$mentor) %>% 
-    v1<-barplot_action() 
-    v1 %>%
-      ggplot() +
-      geom_violin(aes(x =Performance.in.online , y = Time.spent.on.social.media..Hours., 
-                      fill = Do.elderly.people.monitor.you.),trim=FALSE) +
-      labs(x = 'Performance in online', y = "Time spent on social media /Hours.") +
-      ggtitle("Performance in online VS Time spent on social media Hours") +
-      theme_bw() +
-      theme(axis.text.x = element_text(face = 'bold', size = 10),
-            axis.text.y = element_text(face = 'bold', size = 10))
-    
-  }) 
+  })
+
   
   output$barPlot <- renderPlot({
     v<-barplot_action()
@@ -106,18 +84,48 @@ server <- function(input,output){
   
   # economic output
   output$plot5 <- renderPlot({
-    ggplot(df)+
-      geom_bar(aes(x=Performance.in.online,fill = Economic.status))
+    ggplot(data=df, aes(x=Performance.in.online, fill=Economic.status)) +
+      geom_bar( position=position_dodge())+
+      scale_fill_manual(values=c("#9933FF",
+                                 "#33FFFF",
+                                 "red",
+                                 "darkblue"))+
+      theme_dark()
+
   })
   
   # economic2 output
   output$plot6 <- renderPlot({
     ggplot(df)+
-      geom_bar(aes(x=Performance.in.online,fill = Device.type.used.to.attend.classes))
+      geom_line(aes(x=Performance.in.online, color=Device.type.used.to.attend.classes),stat="count")+
+      scale_color_manual(values=c("#CAFF70", "#FF69B4", "#56B4E9"))+
+      theme_dark()
+  }) 
+  
+  # Personal output1
+  output$plot7 <- renderPlotly({
+    plot_ly(data = df2,x=~Time.spent.on.social.media..Hours.,
+            y=~Performance.During.Pandemic,type = "scatter",mode = "markers",
+            colors="pink",marker=list(size=df2$Count,opacity=0.5))
+    
+  })
+  
+  # Personal output2
+  output$plot8 <- renderPlot({
+    #Personalfactor"Effect of Supervision on Education Performance"
+    Supervision <- df$Do.elderly.people.monitor.you.
+    After <- df$Performance.in.online
+    tb6 <- rbind(Supervision,After)
+    tb6 <- xtabs(~ Supervision+After,data=tb6)
+    tb6
+    barplot(tb6,beside = T,xlab = "Value",ylab = "Online Education Performance",
+            main = "Effect of Supervision on Education Performance",
+            col=colors()[c(13,36)],legend.text = TRUE,
+            args.legend = list(x="topright", bty = "n", inset=c(-0.15, 1)),horiz = TRUE)
   }) 
   
   
-  #demographics tab Bar chart
+  #relationship tab Bar chart
   action<-eventReactive(input$action,{filter(df,(Gender==input$Gender & Level.of.Education ==input$education_level & Home.Location==input$hometown & Economic.status==input$economic_status ))})
   
  # two_variable<-eventReactive()
@@ -125,9 +133,7 @@ server <- function(input,output){
   #four_variable<-
   output$dPlot <- renderPlot({
     v<-action()
-    
-    
-    
+  
     # draw the barplot 
     v %>%  group_by(Performance.in.online) %>% summarise(N=n()) %>%
       ggplot(aes(x=Performance.in.online,y=N,fill=Performance.in.online))+
--- Education Shiny/ui.R
+++ Education Shiny/ui.R
@@ -5,7 +5,12 @@ library(shinydashboard)
 library(shinythemes)
 library(DT)
 library(dashboardthemes)
+library(plotly)
+
 df=read.csv("Data/ONLINE EDUCATION SYSTEM REVIEW.csv",stringsAsFactors = TRUE)
+df1=read.csv("Data/before vs after.csv",stringsAsFactors = TRUE)
+df2=read.csv("Data/SocialMedia vs Performance.csv",stringsAsFactors = TRUE)
+
 
 df$Gender<- as.factor(df$Gender)
 df$Performance.in.online<- as.factor(df$Performance.in.online)
@@ -29,10 +34,10 @@ ui <- shinyUI(dashboardPage( skin = 'blue',
                                  id = "sidebar",
                                  
                                  #first menu item
-                                 menuItem(text = "Overview", tabName="home", icon = icon("graduation-cap")),
+                                 menuItem(text = "About Dashboard", tabName="A_Dashboard", icon = icon("home")),
                                  
-                                 #second menu item 
-                                 menuItem(text = "performance", tabName="a_tab", icon = icon("chart-bar")),
+                                 #second menu item
+                                 menuItem(text = "Overview", tabName="home", icon = icon("graduation-cap")),
                                  
                                  #third menu item
                                  menuItem(text = "Economic", tabName="E_chart", icon = icon("chart-bar")),
@@ -40,8 +45,11 @@ ui <- shinyUI(dashboardPage( skin = 'blue',
                                  #fourth menu item
                                  menuItem(text = "Personal", tabName="P_chart", icon = icon("address-book")),
                                  
+                                 #fifth menu item
+                                 menuItem(text = "Relationship", tabName="R_chart", icon = icon("cog")),
+                                 
                                  #about data item
-                                 menuItem(text = "About Data", tabName="about", icon = icon("table"))
+                                 menuItem(text = "About Us", tabName="about", icon = icon("table"))
                                  
                                  
                                )
@@ -51,20 +59,85 @@ ui <- shinyUI(dashboardPage( skin = 'blue',
                                shinyDashboardThemes(
                                 theme = "purple_gradient"
                               ),
+
                                tabItems(
+                                 #The about dashboard tab
+                                 tabItem('A_Dashboard',
+                                         fluidRow(
+                                           mainPanel(align = 'left',
+                                                     
+                                                     img(src='onlineclasses.jpg', width = 600, height = 300, align = 'Center'),
+                                                     br(),
+                                                     br()
+                                                     ),
+                                           tabBox(
+                                             
+                                             # The id lets us use input$tabset1 on the server to find the current tab
+                                             id = "tabset1", height = "450px",
+                                             tabPanel("About Dashboard", 
+                                                      h1("About Dashoard"),
+                                                      p("COVID-19 have brought significant disruptions to the education globally. World-wide pandemic is giving rise to learning losses and increases in inequality. \n
+              To reduce the impact of pandemic to the education, education institutions are forced to begin remote. \n
+              Remote learning brings more convinient as compared to traditional learning, students can access information from anywhere and anytime. Online education is flexible and can be tailored to different needs. \n
+              However, online learning can be possible of lacking control and requires students to be self-disciplined. ", style =  "font-size:15px;"),
+                                                      br(),
+                                                      p("This dashboard is designed to understand how well students nowadays able to cope with the new learning method.\n
+                                                     Analysis of the educational data showing the relationship of each factors and how the factors affecting student online learning performance.", style =  "font-size:15px;")
+                                                      ),
+                                             tabPanel("Overview", 
+                                                      h1("Overview"),
+                                                      p("In this section shows the analytic plots of online education performance by different categories.\n", style =  "font-size:15px;"),
+                                                      p("- Comparison of student education performance before and after pandemic", style =  "font-size:15px;"),
+                                                      p("- Online education performance by education level", style =  "font-size:15px;"),
+                                                      p("- Online education performance by gender", style =  "font-size:15px;")
+                                                      ),
+                                             
+                                             tabPanel("Economic", 
+                                                      h1("Economic"),
+                                                      p("In this section you can discover how economic factors affecting student performance during pandemic.",style =  "font-size:15px;"),
+                                                      p("- Effect of economic status on student's online education performance.",style =  "font-size:15px;"),
+                                                      p("- Effect of device used during online classes on student's performance.",style =  "font-size:15px;")),
+                                             
+                                             tabPanel("Personal", 
+                                                      h1("Personal"),
+                                                      p("In this tab showing how student personal factors affecting their online education performance.",style =  "font-size:15px;"),
+                                                      p("- Relationship between time spent on social media and study performance.",style =  "font-size:15px;"),
+                                                      p("- Comparison of student performance with or without supervision during online class.",style =  "font-size:15px;")
+                                                      ),
+                                                      
+                                             tabPanel("Relationship", 
+                                                      h1("Relationship"),
+                                                      p("Visuallization of relationship betweeen each factors. ",style =  "font-size:15px;"),
+                                                      p("Select parameters for analysis.",style =  "font-size:15px;"),
+                                                      ),
+                                             
+                                             tabPanel("About Us", 
+                                                      h1("About Us"),
+                                                      p("Introduction of the team.",style =  "font-size:15px;"),
+                                                      
+                                             )
+                                             
+                                           )
+                                           )
+                                         ),
+                                 
+
+                                 
+                                         
+
                                  #the Overview tab
                                  tabItem('home',
                                          fluidRow(
                                            box(
-                                             title = "" , width = 12
+                                             title = "Comparison of Education Performance Before and During Pandemic" , width = 12
                                              ,status = "primary"
                                              ,solidHeader = FALSE 
                                              ,collapsible = TRUE 
-                                             ,plotOutput("plot1", height = "300px")
+                                             ,plotlyOutput("plot1", height = "300px")
                                            ),
                                            
                                            box(
-                                             title = ""
+                                             title = "Performance by Education Level"
                                              ,status = "primary"
                                              ,solidHeader = FALSE 
                                              ,collapsible = TRUE 
@@ -81,53 +154,31 @@ ui <- shinyUI(dashboardPage( skin = 'blue',
                                            )
                                          )
                                          ),
-                                 
-                                 
-                                 #first performance tab
-                                 tabItem('a_tab',
-                                         titlePanel("Performance Online and Time Spend on Social Media"), 
-                                         sidebarLayout(
-                                           
-                                           sidebarPanel(
-                                             p(" Do students have mentors and is there a relationship between time spent on social media and online performance?"),
-                                             br(),
-                                             br(),
-                                             selectInput("mentor","Having a mentor",choices=mentor),
-                                             selectInput("gender","Gender",choices=Gender),
-                                             actionButton("plot","plot")
-                                           ),
-                                           
-                                           mainPanel(
-                                             plotOutput("violin"),
-                                             
-                                             plotOutput("barPlot")
-                                             
-                                           )
-                                         )
-                                 ),
-                                 
-                              
-                                 #the about tab
+
+                                 #the about us tab
                                  tabItem('about',
                                          mainPanel(align = 'Center',
                                                    # h1('Data', style = "font-size:80px;" ),
-                                                   br(),
                                                    img(src='um_logo.png', width = 360, height = 120, align = 'Center'),
-                                                   br(),
                                                    br(),
                                                    br(),
                                                    p("Hi!!!This WQD 7001 group project.\n", style = "font-size:29px;"),
                                                    br(),
                                                    p("Welcome to our shiny app！！！ Here you will find out about student performance in online education",style = "font-size:24px;"),
                                                    br(),
-                                                   p("Our team members: \n", style = "font-size:20px;"),
+                                                   p("***********Our team members*********** \n", style = "font-size:20px;"),
                                                    br(),
-                                                   img(src='Team.png', width = 960, height = 260, align = 'Center'),
+                                                   p("--Linsheng Ran"),
+                                                   p("--Weng Jon Lee"),
+                                                   p("--Wen Si"),
+                                                   p("--Chia Siew Lang"),
+                                                   p("--Tuan Nurul Ain Afiqah Binti Tuan Chik"),
                                                    br(),
                                                    tags$a(href="https://www.kaggle.com/datasets/sujaradha/online-education-system-review?select=ONLINE+EDUCATION+SYSTEM+REVIEW.csv", "For the Data"),
                                                    br(),
-                                                   tags$a(href="https://github.com/ranlinsheng/Online-Performance-Dashboard", "GitHub"),
+                                                   tags$a(href="https://github.com/ranlinsheng/Online-Education-Performance-Dashboard", "GitHub"),
                                                    br(),
+                                                   
 
                                          )),
                                  
@@ -154,14 +205,36 @@ ui <- shinyUI(dashboardPage( skin = 'blue',
                                          )
                                          
                                  ),
+                                 #the Personal tab
                                  
-                                 
-                                 #Personal tab
                                  tabItem('P_chart',
-                                         titlePanel("Personal Barchart"),
+                                         fluidRow(
+                                           box(
+                                             title = "Effect of Time Spent on Social Media on Education " , width = 12
+                                             ,status = "primary"
+                                             ,solidHeader = FALSE 
+                                             ,collapsible = TRUE 
+                                             ,plotlyOutput("plot7", height = "300px")
+                                           ),
+                                           
+                                           box(
+                                             title = "Effect of Supervision on Education Performance",width = 12
+                                             ,status = "primary"
+                                             ,solidHeader = FALSE 
+                                             ,collapsible = TRUE 
+                                             ,plotOutput("plot8", height = "300px")
+                                           )
+                                         )
+                                         
+                                 ),
+                                 
+                                 
+                                 #relationship tab
+                                 tabItem('R_chart',
+                                         titlePanel("Relationship Barchart"),
                                          sidebarLayout(
                                            sidebarPanel(
-                                             p(" To understand distribution of online performance related to personal information"),
+                                             p(" To understand how each parameters affecting students’ online learning performance"),
                                              br(),
                                              
                                              selectInput("Gender","Gender",choices= Gender),
